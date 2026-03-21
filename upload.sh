e922faa6c1dcf892568700a19b708269ee10a69c
baki69273-svg <baki69273@gmail.com>
2026-03-22 00:13:28 +0700

    upload 1774113208_chrome_zONHer6k5h.png

--- upload.sh
+++ upload.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# 🔥 QUAN TRỌNG: chuyển vào repo
+cd /d/GitHub_Images/SuiopLnhc-gH271-921-uie-57
+
 img="$1"
+
+[ ! -f "$img" ] && echo "File not found!" && exit 1
 
 filename=$(date +%s)_$(basename "$img")
 
@@ -9,9 +14,10 @@ cp "$img" "images/$filename"
 
 git add .
 git commit -m "upload $filename"
+
+git pull --rebase origin main
 git push
 
 url="https://raw.githubusercontent.com/baki69273-svg/SuiopLnhc-gH271-921-uie-57/main/images/$filename"
 
 echo "$url"
-
