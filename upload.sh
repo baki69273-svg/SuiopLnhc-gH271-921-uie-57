f603fc1d49f9dc97dd4ab7cc80358b2548e630b8
baki69273-svg <baki69273@gmail.com>
2026-03-22 14:27:03 +0700

    upload 1774164423_chrome_6JgfACLTtO.png

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# luôn vào đúng repo (fix lỗi .git)
+# vào đúng repo
 cd /d/GitHub_Images/SuiopLnhc-gH271-921-uie-57
 
 img="$1"
@@ -8,26 +8,23 @@ img="$1"
 # kiểm tra file
 [ ! -f "$img" ] && echo "File not found!" && exit 1
 
+# tạo tên file mới với timestamp
 filename=$(date +%s)_$(basename "$img")
 
 mkdir -p images
 cp "$img" "images/$filename"
 
+# commit & push
 git add .
 git commit -m "upload $filename"
-
 git pull --rebase origin main
 git push
 
-# Bỏ tracking file trong Git để có thể xóa local mà không ảnh hưởng GitHub
-git rm --cached "images/$filename"
-
-# Xóa file gốc local (nếu muốn)
-rm "$img"
-
+# tạo URL
 url="https://raw.githubusercontent.com/baki69273-svg/SuiopLnhc-gH271-921-uie-57/main/images/$filename"
 
-echo "$url"
-
-# copy clipboard (fix bước cuối)
+# copy vào clipboard
 echo "$url" | /c/Windows/System32/clip.exe
+
+# hiển thị URL
+echo "$url"
