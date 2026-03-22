85b69d0c6beac57f6e5195e9194468db2bac7454
baki69273-svg <baki69273@gmail.com>
2026-03-22 14:20:54 +0700

    upload 1774164053_chrome_rgmOFB3szk.png

--- upload.sh
+++ upload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# 🔥 QUAN TRỌNG: chuyển vào repo
+# luôn vào đúng repo (fix lỗi .git)
 cd /d/GitHub_Images/SuiopLnhc-gH271-921-uie-57
 
 img="$1"
 
+# kiểm tra file
 [ ! -f "$img" ] && echo "File not found!" && exit 1
 
 filename=$(date +%s)_$(basename "$img")
@@ -18,8 +19,15 @@ git commit -m "upload $filename"
 git pull --rebase origin main
 git push
 
+# Bỏ tracking file trong Git để có thể xóa local mà không ảnh hưởng GitHub
+git rm --cached "images/$filename"
+
+# Xóa file gốc local (nếu muốn)
+rm "$img"
+
 url="https://raw.githubusercontent.com/baki69273-svg/SuiopLnhc-gH271-921-uie-57/main/images/$filename"
 
 echo "$url"
-echo "$url" | clip
 
+# copy clipboard (fix bước cuối)
+echo "$url" | /c/Windows/System32/clip.exe
